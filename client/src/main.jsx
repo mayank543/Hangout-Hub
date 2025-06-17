4ac117887fc62d4a055f3292c23814b0a3048148
mayank543 <mayankdoholiya@gmail.com>
2025-06-17 16:42:40 +0530

    forced redirect to main app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -29,6 +29,7 @@ createRoot(document.getElementById('root')).render(
           <SignUp
             signInUrl="/sign-in"
             afterSignUpUrl="/"
+            forceRedirectUrl="/"
           />
         }
       />
