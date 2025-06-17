b84d0f7d0de351af5852f235e30ac59c7cf9b0c6
mayank543 <mayankdoholiya@gmail.com>
2025-06-17 16:52:10 +0530

    same for sign in

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -19,6 +19,7 @@ createRoot(document.getElementById('root')).render(
           <SignIn
             signUpUrl="/sign-up"
             afterSignInUrl="/"
+            forceRedirectUrl="/"
           />
         }
       />
